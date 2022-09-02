70b875f5b90ff29d157a4f83505425f77e9ca8d4
zohidjonakbarov <zohidjonakbarov@gmail.com>
2022-09-02 06:17:24 +0500

    Initial commit

---
+++ app/src/main/java/iqro/mobile/animations/MainActivity.kt
@@ -0,0 +1,44 @@
+package iqro.mobile.animations
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.animation.AnimationUtils
+import iqro.mobile.animations.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.alphaBtn.setOnClickListener {
+
+            val animation = AnimationUtils.loadAnimation(this, R.anim.alpha_anim)
+            binding.imageView.startAnimation(animation)
+
+        }
+        binding.rotateBtn.setOnClickListener {
+            val animation = AnimationUtils.loadAnimation(this, R.anim.rotate_anim)
+            binding.imageView.startAnimation(animation)
+        }
+
+        binding.translateBtn.setOnClickListener {
+            val animation = AnimationUtils.loadAnimation(this, R.anim.translate_anim)
+            binding.imageView.startAnimation(animation)
+        }
+
+        binding.scaleBtn.setOnClickListener {
+            val animation = AnimationUtils.loadAnimation(this, R.anim.scale_anim)
+            binding.imageView.startAnimation(animation)
+        }
+
+        binding.combinationBtn.setOnClickListener {
+            val animation = AnimationUtils.loadAnimation(this, R.anim.combination_anim)
+            binding.imageView.startAnimation(animation)
+        }
+
+    }
+}
